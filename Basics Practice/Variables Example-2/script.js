aa164f7c8a97e7e3ad1262251f7579bf4e6299f3
Rahul Charan <rahulcharan0190@outlook.com>
2018-06-01 07:55:48 +0530

    Simple Type Coersion and variable nature Example

---
+++ Basics Practice/Variables Example-2/script.js
@@ -0,0 +1,10 @@
+/*This is simple program demonstrating type conversion.
+  + operator can be used for concatination of values.
+  var is capable of holding any type of value.
+*/
+var name,age,job,isMarried;
+name='Rahul Charan';
+age=27;
+job='Software Engineer';
+isMarried=false;
+console.log(name+' is a '+age+' years old '+job+'. is He Married? '+isMarried+'.');
