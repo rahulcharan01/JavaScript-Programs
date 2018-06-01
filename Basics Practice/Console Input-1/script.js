7475dd0057b329b993c357e8493b53efbff459bc
Rahul Charan <rahulcharan0190@outlook.com>
2018-06-01 08:03:36 +0530

    Simple Console Input Example

---
+++ Basics Practice/Console Input-1/script.js
@@ -0,0 +1,7 @@
+/*This is example of taking console input.
+  Though it is not very useful in real time situations. 
+  But it helps a lot in debugging*/
+var firstName,lastName;
+firstName=prompt('What is your first name?');
+lastName=prompt('What is your last name');
+console.log('Full Name is:'+firstName+' '+lastName);
